e0801a677821b3e56878ae84a8e4981ad213c3df
Astwen <astrid.wennberg@hotmail.se>
2016-05-02 13:28:04 +0200

    Update repetition.js

--- repetition.js
+++ repetition.js
@@ -6,3 +6,21 @@ var weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "
 
 console.log("Exempel 1. Första veckodagen " + weekdays[0]);
 
+
+var month = [];
+month[0] = "Januari";
+month[1] = "Februari";
+month[2] = "Mars";
+month[3] = "April";
+month[4] = "Maj";
+month[5] = "Juni";
+month[6] = "Juli";
+month[7] = "Augusti";
+month[8] = "September";
+month[9] = "Oktober";
+month[10] = "November";
+month[11] = "December";
+
+console.log("Månad" + month[1]);
+console.log("Jag är född i " + month[1]);
+
