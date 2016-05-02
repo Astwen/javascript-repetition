543402aa76377163a87839f40231dd4deee24ae7
Astwen <astrid.wennberg@hotmail.se>
2016-05-02 13:15:30 +0200

    Create repetition.js

---
+++ repetition.js
@@ -0,0 +1,8 @@
+// JavaScript
+
+var weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
+
+// Hämta ur vår array
+
+console.log("Exempel 1. Första veckodagen " + weekday[0]);
+
