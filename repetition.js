6803055b519109de14b424cc037f668ebaab3948
Astwen <astrid.wennberg@hotmail.se>
2016-05-02 13:56:37 +0200

    Update repetition.js

--- repetition.js
+++ repetition.js
@@ -24,3 +24,38 @@ month[11] = "December";
 console.log("Månad" + month[4]);
 console.log("Jag är född i " + month[2]);
 
+var car = {color: "ful" hasAWheel: true, numberOfSeats: 4};
+
+var computer = {color: "grey", brand: "Mac"};
+
+var student = {};
+student.name = "Anton";
+student.favoriteTeam = "AIK";
+
+var anotherStudent = {};
+student["name"] = "Oskar";
+student["favoriteTeam"] = "HIF";
+
+console.log ("Den första studenten heter" + student.name);
+console.log ("Den andra studenten heter" + student["name"]);
+
+var students = [student, anotherStudent];
+
+console.log("Första studenten i array: " + students[0].name);
+console.log("Andra studenten i array: " + students[1].name);
+
+var favoriteGames = [["Super Mario Bros 1", "Duck Hunt"],["Wii Sports Resort", "Zelda Twilight Princess"]];
+
+console.log("Andra spelet i första listan" + favoriteGames [0][1]);
+
+var superMario64 = {title: "Super Mario 64", countries: ["Sverige", "USA"], characters: ["Mario", "Toad", "Peach"]}
+
+console.log ("Första karaktären är " + superMario64.characters[0]);
+console.log ("Andra landet är " + sperMario64["countries"][1]);
+
+var soccerPlayer = {name: "Zlatan", orginalClub: {name: "Malmö FF", playedInChampionsLeauge: true, founded: 1901}};
+
+
+console.log("Året då Zlatans orginalklubb startades " + soccerPlayer.originalClub.founded);
+console.log("Zlatans orginalklubb heter " + soccerPlayer[originalClub][founded]);
+
